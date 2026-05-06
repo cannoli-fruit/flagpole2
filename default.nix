26e438843d5f47563b4ad981d1d2f0be84d5224d
Cody <co.dy.0.i.s.0.he.re@gmail.com>
2026-05-06 11:02:40 -0400

    vibe coded a nix wrappre for this, idk how nix works

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ stdenv, makeWrapper ? null, writeShellScriptBin ? null }:
+
+stdenv.mkDerivation rec {
+  pname = "flagpole";
+  version = "1.0"; # bump as needed
+
+  src = ./.;
+
+  nativeBuildInputs = [];
+  buildInputs = [];
+
+  # If you want wrapper helpers (optional)
+  propagatedBuildInputs = [];
+
+  installPhase = ''
+    mkdir -p $out/lib/flagpole
+    cp ${src}/setup.ll $out/lib/flagpole/setup.ll
+    cp ${src}/end.ll $out/lib/flagpole/end.ll
+    mkdir -p $out/lib/flagpole/lib
+    cp -r ${src}/lib/* $out/lib/flagpole/
+    cp ${src}/llvm.py $out/lib/flagpole/fpcc
+    chmod +x $out/lib/flagpole/fpcc
+
+    mkdir -p $out/bin
+    cp ${src}/compiler.sh $out/bin/fpc
+    chmod +x $out/bin/fpc
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Flagpole compiler/runtime";
+    license = licenses.mit; # set to correct license
+    maintainers = with maintainers; [];
+  };
+}
